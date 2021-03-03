73eb1809d5ad0685fa235de3b0b0147fa6ca4d39
ColbyWTaylor <colbyt.093@gmail.com>
2021-03-03 11:21:08 -0500

    profile update

--- src/Users.js
+++ src/Users.js
@@ -7,19 +7,28 @@ const Button = styled.button`
   color: white;
 `;
 
-const Users = () => {
+const ProfilePic = styled.img`
+  width: 300px;
+`;
+
+function Users() {
   return (
     <div className="nav">
       <h1>Users</h1>
       <p>Here are a list of users</p>
       <div className="userCard">
         <h3>Name</h3>
-        <p>I like bagels</p>
+        <ProfilePic
+          src={"profile-pic-default.png"}
+          alt={"default profile pic"}
+        />
+        <p>Dr. Bob Hanki Wanker</p>
+        <p>so you trust me yet?</p>
       </div>
 
       <Button>If I were green I would die</Button>
     </div>
   );
-};
+}
 
 export default Users;
